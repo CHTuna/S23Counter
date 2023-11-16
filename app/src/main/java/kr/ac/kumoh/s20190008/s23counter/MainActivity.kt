5514f00b81cf7554bf39721ca9c4d78a21e1e54a
EIVE <45841115+CHTuna@users.noreply.github.com>
2023-11-16 13:44:54 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/kumoh/s20190008/s23counter/MainActivity.kt
@@ -0,0 +1,71 @@
+package kr.ac.kumoh.s20190008.s23counter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kr.ac.kumoh.s20190008.s23counter.ui.theme.S23CounterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp {
+                Clicker()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit){
+    S23CounterTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun Clicker() {
+    var (txtString, setTxtString) = remember { mutableStateOf("눌러주세요") }
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(8.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = txtString,
+            fontSize = 70.sp,
+        )
+        Button(modifier = Modifier
+            .fillMaxWidth(),
+            onClick = {
+                setTxtString("눌렸습니다")
+            }) {
+            // Text(text = "눌러봐")
+            Text("눌러봐")
+        }
+    }
+}
